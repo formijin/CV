1b96272fb84e5d8e7c60b9b13f0ad9f01279ec2d
formijin <fortunate.nneji>
2023-10-04 15:44:27 +0100

    Added backend server

---
+++ index.js
@@ -0,0 +1,19 @@
+import  express  from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app =express();
+const port =3000;
+
+app.use(express.static(`${__dirname}/public`));
+
+
+app.get("/",(req,res)=>{
+    res.sendfile(`${__dirname}/index.html`)
+});
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
