d539868481ea037af310a4bac37c586a57ee1e88
formijin <fortunate.nneji>
2023-10-12 20:49:30 +0100

    create views folder

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(`${__dirname}/public`));
 
 
 app.get("/",(req,res)=>{
-    res.sendfile(`${__dirname}/index.html`)
+    res.sendfile(`${__dirname}/views/index.html`);
 });
 
 
