c0cc1063a6732bf4f3f864a305921b45ab5aa42a
formijin <fortunate.nneji>
2023-10-12 21:06:16 +0100

    title case on piublic in server

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app =express();
 const port =3000;
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/Public`));
 
 
 app.get("/",(req,res)=>{
